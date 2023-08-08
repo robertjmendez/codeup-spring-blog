1bbd79eccf3cc9df7a942f495d0f95bb93ba4352
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-08-08 12:38:38 -0500

    Created the file structure for the JPA exercise

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,34 +1,52 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.model.Post;
+import com.codeup.codeupspringblog.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+import java.util.Optional;
+
 @Controller
 public class PostController {
-    @ResponseBody
+
+    private final PostRepository postDao;
+
+    public PostController(PostRepository postDao) {
+        this.postDao = postDao;
+    }
+
     @GetMapping("/posts")
-    public String index() {
-        return "posts index page";
+    public String allPosts(Model model) {
+        model.addAttribute("posts", postDao.findAll());
+        return "posts/index";
     }
 
-    @ResponseBody
     @GetMapping("/posts/{id}")
-    public String viewPost(@PathVariable String id) {
-        return "view an individual post with id: " + id;
+    public String individualPost(@PathVariable long id, Model model) {
+        Optional<Post> postOptional = postDao.findById(id);
+        if (postOptional.isPresent()) {
+            model.addAttribute("post", postOptional.get());
+            return "posts/show";
+        } else {
+            return "redirect:/posts";
+        }
     }
 
-    @ResponseBody
     @GetMapping("/posts/create")
-    public String viewCreateForm() {
-        return "view the form for creating a post";
+    public String viewCreateForm(Model model) {
+        model.addAttribute("post", new Post());
+        return "posts/create";
     }
 
-    @ResponseBody
     @PostMapping("/posts/create")
-    public String createPost() {
-        return "create a new post";
+    public String createPost(Post post) {
+        postDao.save(post);
+        return "redirect:/posts";
     }
 }
---
+++ src/main/java/com/codeup/codeupspringblog/model/Post.java
@@ -0,0 +1,55 @@
+package com.codeup.codeupspringblog.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="posts")
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false)
+    private String body;
+
+    public Post() {
+    }
+
+    public Post(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+    public Post(Long id, String title, String body) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+}
