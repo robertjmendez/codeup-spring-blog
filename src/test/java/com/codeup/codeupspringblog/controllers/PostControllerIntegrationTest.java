a730560c949d74e4ee232aa43a443af9771f4cd0
robertjmendez <98974225+robertjmendez@users.noreply.github.com>
2023-08-15 19:07:34 -0500

    Working on the integration test

--- src/test/java/com/codeup/codeupspringblog/controllers/PostControllerIntegrationTest.java
+++ src/test/java/com/codeup/codeupspringblog/controllers/PostControllerIntegrationTest.java
@@ -1,13 +1,33 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.model.Post;
 import com.codeup.codeupspringblog.repositories.PostRepository;
 import com.codeup.codeupspringblog.repositories.UserRepository;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.containsString;
+
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,4 +42,40 @@ public class PostControllerIntegrationTest {
 
     @Autowired
     private UserRepository userDao;
+
+    private MockHttpSession httpSession;
+
+    @Before
+    public void setup() {
+        httpSession = new MockHttpSession();
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("robert")
+                .password("12345")
+                .roles("")
+                .build();
+        Authentication auth = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(auth);
+    }
+
+
+    @Test
+    public void testPostsIndex() throws Exception {
+        // Makes a Get request to /posts and verifies that the posts index page is rendered correctly
+        this.mvc.perform(get("/posts"))
+                .andExpect(status().isOk())
+                // Test the static content of the page
+                .andExpect(content().string(containsString("All Posts")));
+    }
+
+    @Test
+    public void testShowPost() throws Exception {
+        Post existingPost = postDao.findAll().get(0);
+
+        // Makes a Get request to /posts/{id} and expects the post details page to be rendered correctly
+        this.mvc.perform(get("/posts/" + existingPost.getId()))
+                .andExpect(status().isOk())
+                // Test the dynamic content of the page
+                .andExpect(content().string(containsString(existingPost.getTitle())))
+                .andExpect(content().string(containsString(existingPost.getBody())));
+    }
 }
